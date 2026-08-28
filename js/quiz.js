46bf24c5946325617af29cb4bd0c8f3eda55dbfa
Fiona Friberg <fionafriberg@outlook.com>
2026-08-28 17:02:42 +0200

    make buttons for each answer

--- js/quiz.js
+++ js/quiz.js
@@ -7,7 +7,8 @@ async function loadQuiz (){
 
      const data = await response.json(); //Läs innehållet i response och tolka det som json
 
-     const element = document.querySelector("#quiz");
+     const element = document.querySelector("#question");
+     const elAnswers = document.querySelector("#answers")
 
 
 
@@ -17,7 +18,19 @@ async function loadQuiz (){
      data.questions.forEach(question => {
         element.textContent = question.question;
 
+
+        question.answers.forEach(answer => {
+        const button = document.createElement("button");
+
+        button.textContent = answer;
+
+        elAnswers.append(button);
+
+     })
+
      });
+
+     
 
     console.log(data.characters);
 }
